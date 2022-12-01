82a5473b58c5ac589043adc34470b87bac6b9db3
Thomas Hoffmann <thomas.hoffmann@rewe-digital.com>
2022-12-01 09:22:00 +0100

    day1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,17 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    data class Elf(val calories: Int)
+
+    fun getElves(input: List<String>) = input.separateBy { it.isBlank() }.map { list -> Elf(list.sumOf { it.toInt() }) }
+
+    fun part1(input: List<String>) = getElves(input).maxBy { it.calories }.calories
+
+    fun part2(input: List<String>) = getElves(input).sortedByDescending { it.calories }.take(3).sumOf { it.calories }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
--- src/Utils.kt
+++ src/Utils.kt
@@ -14,3 +14,25 @@ fun readInput(name: String) = File("src", "$name.txt")
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
     .toString(16)
     .padStart(32, '0')
+
+/**
+ * Splits a list into a list of lists by separating the original elements whenever an element matches the given predicate.
+ * The matched element is *NOT* added to any sublist!
+ *
+ * @param predicate lambda to apply on an element to test if it should be a separator element
+ * @return the list split into a list of lists by separating them at specific separator elements
+ */
+fun <T> List<T>.separateBy(predicate: (T) -> Boolean): List<List<T>> {
+    val list = mutableListOf<List<T>>()
+    var sublist = mutableListOf<T>()
+    forEach {
+        if (predicate(it)) {
+            list.add(sublist)
+            sublist = mutableListOf()
+        } else {
+            sublist.add(it)
+        }
+    }
+    list.add(sublist)
+    return list
+}
