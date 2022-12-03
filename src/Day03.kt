2f561fe7cf019ee3ca072fe0b659678a52beb286
Thomas Hoffmann <git@j4velin.de>
2022-12-03 10:50:00 +0100

    day3 solution

---
+++ src/Day03.kt
@@ -0,0 +1,40 @@
+fun findCommonItem(vararg compartments: String): Char {
+    var intersect = compartments.first().toCharArray().toSet()
+    compartments.drop(1).map { it.toCharArray().toSet() }.forEach { intersect = intersect.intersect(it) }
+    return intersect.first()
+}
+
+val Char.priority: Int
+    get() = if (isLowerCase()) {
+        this - 'a' + 1
+    } else {
+        this - 'A' + 27
+    }
+
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        return input.sumOf { rucksack ->
+            val compartment1 = rucksack.substring(0, rucksack.length / 2)
+            val compartment2 = rucksack.substring(rucksack.length / 2)
+            val item = findCommonItem(compartment1, compartment2)
+            item.priority
+        }
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.windowed(3, 3).sumOf { elves ->
+            val item = findCommonItem(elves[0], elves[1], elves[2])
+            item.priority
+        }
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day03_test")
+    check(part1(testInput) == 157)
+    check(part2(testInput) == 70)
+
+    val input = readInput("Day03")
+    println(part1(input))
+    println(part2(input))
+}
