9c530474721e122facfedcc96add55656d9f6c41
Thomas Hoffmann <thomas.hoffmann@rewe-digital.com>
2022-12-06 08:49:48 +0100

    day6 solution

--- src/aoc2022/Day06.kt
+++ src/aoc2022/Day06.kt
@@ -4,19 +4,20 @@ import readInput
 
 fun main() {
 
-	fun part1(input: List<String>) : Int {
-		return 0
-	}
+    fun findMarkerIndex(input: String, distinctCount: Int): Int =
+        input.withIndex().windowed(size = distinctCount, step = 1)
+            .first { l -> l.distinctBy { it.value }.size == l.size }
+            .last().index + 1
 
-	fun part2(input: List<String>) : Int {
-		return 0
-	}
+    fun part1(input: List<String>): Int = findMarkerIndex(input.first(), 4)
 
-	val testInput = readInput("Day06", 2022)
-	check(part1(testInput) == 0)
-	check(part2(testInput) == 0)
+    fun part2(input: List<String>): Int = findMarkerIndex(input.first(), 14)
 
-	val input = readInput("Day06", 2022)
-	println(part1(input))
-	println(part2(input))
+    val testInput = readInput("Day06_test", 2022)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 19)
+
+    val input = readInput("Day06", 2022)
+    println(part1(input))
+    println(part2(input))
 }
