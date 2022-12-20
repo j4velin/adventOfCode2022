7669063521646e1d92ee7c3c41026a6318d3a471
Thomas Hoffmann <git@j4velin.de>
2022-12-20 21:05:57 +0100

    fix day20 pt2

--- src/aoc2022/Day20.kt
+++ src/aoc2022/Day20.kt
@@ -2,41 +2,39 @@ package aoc2022
 
 import readInput
 
-// input contains duplicates...
-data class MixedNumber(val value: Number, val mixCount: Int)
+// input contains duplicates, so keep the original index
+data class MixedNumber(val value: Long, val originalIndex: Int)
 
-private fun mixNumbers(original: LongArray, rounds: Int): List<Number> {
-    val list = original.map { MixedNumber(it, 0) }.toMutableList()
-    repeat(rounds) { round ->
-        original.forEach { number ->
-            val search = MixedNumber(number, round)
-            val oldIndex = list.indexOf(search)
-            list.remove(search)
-            val newIndex = ((((oldIndex + number) % list.size) + list.size) % list.size).toInt()
-            val newElement = MixedNumber(number, round + 1)
-            list.add(newIndex, newElement)
+private fun mixNumbers(original: LongArray, rounds: Int): List<Long> {
+    val list = original.withIndex().map { MixedNumber(it.value, it.index) }.toMutableList()
+    repeat(rounds) {
+        original.indices.forEach { originalIndex ->
+            val oldIndex = list.indexOfFirst { it.originalIndex == originalIndex }
+            val element = list.removeAt(oldIndex)
+            val newIndex = ((((oldIndex + element.value) % list.size) + list.size) % list.size).toInt()
+            list.add(newIndex, element)
         }
     }
     return list.map { it.value }
 }
 
-private fun part1(input: List<String>): Int {
+private fun part1(input: List<String>): Long {
     val array = input.map { it.toLong() }.toLongArray()
     val list = mixNumbers(array, 1)
     val startIndex = list.indexOfFirst { it == 0L }
-    return list[(startIndex + 1000) % list.size].toInt() + list[(startIndex + 2000) % list.size].toInt() + list[(startIndex + 3000) % list.size].toInt()
+    return list[(startIndex + 1000) % list.size] + list[(startIndex + 2000) % list.size] + list[(startIndex + 3000) % list.size]
 }
 
 private fun part2(input: List<String>): Long {
     val array = input.map { it.toLong() * 811589153L }.toLongArray()
     val list = mixNumbers(array, 10)
     val startIndex = list.indexOfFirst { it == 0L }
-    return list[(startIndex + 1000) % list.size].toLong() + list[(startIndex + 2000) % list.size].toLong() + list[(startIndex + 3000) % list.size].toLong()
+    return list[(startIndex + 1000) % list.size] + list[(startIndex + 2000) % list.size] + list[(startIndex + 3000) % list.size]
 }
 
 fun main() {
     val testInput = readInput("Day20_test", 2022)
-    check(part1(testInput) == 3)
+    check(part1(testInput) == 3L)
     check(part2(testInput) == 1623178306L)
 
     val input = readInput("Day20", 2022)
