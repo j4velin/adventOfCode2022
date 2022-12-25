1bb77741d4ceca82b1ed4039e8eb368b87e60e78
Thomas Hoffmann <git@j4velin.de>
2022-12-25 10:14:43 +0100

    day 25 wip

--- src/aoc2022/Day25.kt
+++ src/aoc2022/Day25.kt
@@ -2,22 +2,50 @@ package aoc2022
 
 import readInput
 
-object Day25 {
-	fun part1(input: List<String>): Int {
-		return 0
-	}
+typealias SNAFU = String
 
-	fun part2(input: List<String>): Int {
-		return 0
-	}
+object Day25 {
+
+    private fun Char.digitFromSNAFU(): Int {
+        return when (this) {
+            '=' -> -2
+            '-' -> -1
+            else -> this.digitToInt()
+        }
+    }
+
+    private fun SNAFU.toDecimal(): Long {
+        val array = this.toCharArray()
+        var result = 0L
+        var multiplier = 1
+        for (i in array.size - 1 downTo 0) {
+            val decimalDigit = array[i].digitFromSNAFU()
+            result += decimalDigit * multiplier
+            multiplier *= 5
+        }
+        return result
+    }
+
+    private fun Long.toSNAFU(): String {
+        TODO()
+    }
+
+    fun part1(input: List<String>): String {
+        val decimalSum = input.sumOf { it.toDecimal() }
+        return decimalSum.toSNAFU()
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
 }
 
 fun main() {
-	val testInput = readInput("Day25_test", 2022)
-	check(Day25.part1(testInput) == 0)
-	check(Day25.part2(testInput) == 0)
+    val testInput = readInput("Day25_test", 2022)
+    check(Day25.part1(testInput) == "2=-1=0")
+    check(Day25.part2(testInput) == 0)
 
-	val input = readInput("Day25", 2022)
-	println(Day25.part1(input))
-	println(Day25.part2(input))
+    val input = readInput("Day25", 2022)
+    println(Day25.part1(input))
+    println(Day25.part2(input))
 }
