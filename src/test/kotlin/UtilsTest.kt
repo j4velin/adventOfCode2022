fceaf062d478cb6f9bff172a828a8ffc3f7e9282
Thomas Hoffmann <thomas.hoffmann@rewe-digital.com>
2023-12-04 15:57:36 +0100

    re-enable gradle

--- src/test/kotlin/UtilsTest.kt
+++ src/test/kotlin/UtilsTest.kt
@@ -7,7 +7,7 @@ class UtilsTest {
         val list = listOf(
             "a b c d e f g",
             "h i j k l m n",
-            "o p q r s t u"
+            "o p q r s t u",
             "v w x y z . ."
         )
         val array = list.to2dIntArray()
