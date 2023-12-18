de1b68774cfa5585d3a29bf0d9387a5086354599
Thomas Hoffmann <thomas.hoffmann@rewe-digital.com>
2023-12-18 11:58:30 +0100

    day18 solution

--- src/main/kotlin/aoc2023/Day18.kt
+++ src/main/kotlin/aoc2023/Day18.kt
@@ -1,93 +1,56 @@
 package aoc2023
 
-import Point
+import PointL
 import readInput
 
 @OptIn(ExperimentalStdlibApi::class)
 object Day18 {
-    fun part1(input: List<String>): Int {
-        var current = Point(0, 0)
-        val dugOut = mutableSetOf(current)
-        input.forEach { line ->
-            val split = line.split(" ")
-            val direction = split[0]
-            val length = split[1].toInt()
-            val delta = when (direction) {
-                "U" -> Point(0, -1)
-                "D" -> Point(0, 1)
-                "L" -> Point(-1, 0)
-                "R" -> Point(1, 0)
-                else -> throw IllegalArgumentException("Unknown direction: $direction")
-            }
-            repeat(length) {
-                current += delta
-                dugOut.add(current)
-            }
-        }
-        val maxX = dugOut.maxOf { it.x }
-        val maxY = dugOut.maxOf { it.y }
-        val minX = dugOut.minOf { it.x }
-        val minY = dugOut.minOf { it.y }
-        val width = maxX - minX
-        val height = maxY - minY
-        val map = Array(width + 1) { CharArray(height + 1) { '.' } }
-        dugOut.forEach {
-            map[it.x - minX][it.y - minY] = '#'
+
+    /**
+     * @param input the puzzle input
+     * @param getDelta a function to transform a given input line into a [PointL]-Delta (to get to the next point)
+     */
+    private fun solve(input: List<String>, getDelta: (String) -> PointL): Long {
+        var current = PointL(0L, 0L)
+        val edges = buildList {
+            add(current)
+            addAll(input.map { line -> current += getDelta(line); current })
         }
 
-        val grid = Point(0, 0) to Point(width, height)
-        var changed = true
-        while (changed) {
-            changed = false
-            for (x in 0..width) {
-                for (y in 0..height) {
-                    val value = map[x][y]
-                    if (value == '.') {
-                        val point = Point(x, y)
-                        val neighbours = point.getNeighbours().filter { it.isWithin(grid) }.map { map[it.x][it.y] }
-                        if (neighbours.size < 4 || neighbours.any { it == ' ' }) {
-                            map[x][y] = ' '
-                            changed = true
-                        }
-                    }
-                }
-            }
-        }
+        val shoeLace = edges.windowed(2, 1)
+            .sumOf { points -> (points.first().y + points.last().y) * (points.first().x - points.last().x) } / 2L
 
-        return map.sumOf { column -> column.count { it != ' ' } }
+        val border = edges.windowed(2, 1).sumOf { it.first().distanceTo(it.last()) } / 2
+
+        return shoeLace + border + 1L
     }
 
-    fun part2(input: List<String>): Long {
-        var current = Point(0, 0)
-        val dugOut = mutableListOf(current)
-        input.forEach { line ->
-            // drop '(#' and ')'
-            val color = line.substring(line.lastIndexOf('(')).drop(2).take(6)
-            val direction = color.last()
-            val length = color.take(5).hexToInt()
-            // 0 means R, 1 means D, 2 means L, and 3 means U.
-            current += when (direction) {
-                '3' -> Point(0, -length)
-                '1' -> Point(0, length)
-                '2' -> Point(-length, 0)
-                '0' -> Point(length, 0)
-                else -> throw IllegalArgumentException("Unknown direction: $direction")
-            }
-            dugOut.add(current)
+    fun part1(input: List<String>) = solve(input) { line ->
+        val split = line.split(" ")
+        val direction = split[0]
+        val length = split[1].toLong()
+        when (direction) {
+            "U" -> PointL(0, -length)
+            "D" -> PointL(0, length)
+            "L" -> PointL(-length, 0)
+            "R" -> PointL(length, 0)
+            else -> throw IllegalArgumentException("Unknown direction: $direction")
         }
+    }.toInt()
 
-        val maxX = dugOut.maxOf { it.x }
-        val maxY = dugOut.maxOf { it.y }
-        val minX = dugOut.minOf { it.x }
-        val minY = dugOut.minOf { it.y }
-        val width = maxX - minX
-        val height = maxY - minY
-
-        val total = width.toLong() * height.toLong()
-
-        println("$width x $height -> $total")
-
-        return 0L
+    fun part2(input: List<String>) = solve(input) { line ->
+        // drop '(#' and ')'
+        val color = line.substring(line.lastIndexOf('(')).drop(2).take(6)
+        val direction = color.last()
+        val length = color.take(5).hexToLong()
+        // 0 means R, 1 means D, 2 means L, and 3 means U.
+        when (direction) {
+            '3' -> PointL(0, -length)
+            '1' -> PointL(0, length)
+            '2' -> PointL(-length, 0)
+            '0' -> PointL(length, 0)
+            else -> throw IllegalArgumentException("Unknown direction: $direction")
+        }
     }
 }
 
