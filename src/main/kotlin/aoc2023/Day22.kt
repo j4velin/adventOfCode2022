6ff94e83a4ab9c454abae88270662b68114e5b9e
Thomas Hoffmann <git@j4velin.de>
2023-12-22 11:00:21 +0100

    day22 pt2 solution

--- src/main/kotlin/aoc2023/Day22.kt
+++ src/main/kotlin/aoc2023/Day22.kt
@@ -21,20 +21,31 @@ object Day22 {
             }
         }
 
+        /**
+         * Move the brick downwards by the given delta
+         * @param dz the distance to move in z direction
+         */
         fun moveDown(dz: Int) = Brick(start.move(0, 0, -dz), end.move(0, 0, -dz))
 
+        /**
+         * @return true, if this brick lays directly on any of the bricks given in [other]
+         */
         fun laysOnAny(other: Set<Brick>) = other.any { otherBrick ->
             xRange.overlaps(otherBrick.xRange) && yRange.overlaps(otherBrick.yRange) && zRange.first == otherBrick.zRange.last + 1
         }
 
+        /**
+         * @return true, if this brick supports the [other] brick from falling down
+         */
         fun supports(other: Brick) =
             xRange.overlaps(other.xRange) && yRange.overlaps(other.yRange) && zRange.last == other.zRange.first - 1
     }
 
-    fun part1(input: List<String>): Int {
+    private fun getBricksInFinalPosition(input: List<String>): Set<Brick> {
         val fallingBricks = input.map { Brick.fromString(it) }
         val bricksInFinalPosition = mutableSetOf<Brick>()
 
+        // fall down
         fallingBricks.sortedBy { it.zRange.first }.forEach { brick ->
             if (brick.zRange.first == 1L) {
                 bricksInFinalPosition.add(brick)
@@ -46,32 +57,63 @@ object Day22 {
                 bricksInFinalPosition.add(tmpBrick)
             }
         }
+        return bricksInFinalPosition
+    }
 
-        val bricksSafeToRemove = bricksInFinalPosition.filter { brick ->
-            val bricksSupportedByTheCurrentBrick =
-                bricksInFinalPosition.filter { it.zRange.first == brick.zRange.last + 1 }.filter { brick.supports(it) }
-            val possibleOtherSupports =
-                bricksInFinalPosition.filter { it != brick && it.zRange.last == brick.zRange.last }
+    /**
+     * @return bricks, which are only supported by [toTest] (e.g. which would fall down if [toTest] was removed)
+     */
+    private fun getDependantBricks(toTest: Brick, bricks: Collection<Brick>): Collection<Brick> {
+        val bricksSupportedByTheCurrentBrick =
+            bricks.filter { it.zRange.first == toTest.zRange.last + 1 }.filter { toTest.supports(it) }
+        val possibleOtherSupports =
+            bricks.filter { it != toTest && it.zRange.last == toTest.zRange.last }
 
-            val unsupportedBricks = bricksSupportedByTheCurrentBrick.filter { brickToTest ->
-                possibleOtherSupports.none { support -> support.supports(brickToTest) }
-            }
-
-            unsupportedBricks.isEmpty()
+        val unsupportedBricks = bricksSupportedByTheCurrentBrick.filter { brickToTest ->
+            possibleOtherSupports.none { support -> support.supports(brickToTest) }
         }
+        return unsupportedBricks
+    }
 
+    /**
+     * Tests, how many bricks would fall when removing a set of bricks.
+     *
+     * @param removedBricks the bricks to remove
+     * @param remainingBricks the bricks, which are still standing (all bricks except those in [removedBricks])
+     * @return the number of bricks, which would fall further down if the bricks in [removedBricks] are removed
+     */
+    private fun wouldFall(removedBricks: Set<Brick>, remainingBricks: Collection<Brick>): Int {
+        val unsupportedBricks =
+            remainingBricks.filter { brick -> brick.zRange.first > 1 && remainingBricks.none { it.supports(brick) } }
+        return if (unsupportedBricks.isNotEmpty()) {
+            wouldFall(removedBricks + unsupportedBricks, remainingBricks.filter { it !in removedBricks })
+        } else {
+            removedBricks.size
+        }
+    }
+
+    fun part1(input: List<String>): Int {
+        val allBricks = getBricksInFinalPosition(input)
+        val bricksSafeToRemove = allBricks.filter { brick -> getDependantBricks(brick, allBricks).isEmpty() }
         return bricksSafeToRemove.size
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val allBricks = getBricksInFinalPosition(input)
+        val bricksUnSafeToRemove = allBricks.filter { brick -> getDependantBricks(brick, allBricks).isNotEmpty() }
+
+        // fast enough for the puzzle input, no need to optimize
+        return bricksUnSafeToRemove.parallelStream().map { brickToRemove ->
+            // -1 because we must not count the 'brickToRemove' itself
+            wouldFall(setOf(brickToRemove), allBricks.filter { it != brickToRemove }) - 1
+        }.reduce(0) { a, b -> a + b }
     }
 }
 
 fun main() {
     val testInput = readInput("Day22_test", 2023)
     check(Day22.part1(testInput) == 5)
-    check(Day22.part2(testInput) == 0)
+    check(Day22.part2(testInput) == 7)
 
     val input = readInput("Day22", 2023)
     println(Day22.part1(input))
