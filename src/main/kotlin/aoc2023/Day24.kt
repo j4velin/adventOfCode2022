d28e0bf4cc320906b8381e509e220b502bf93de0
Thomas Hoffmann <git@j4velin.de>
2023-12-24 10:11:41 +0100

    refactor

--- src/main/kotlin/aoc2023/Day24.kt
+++ src/main/kotlin/aoc2023/Day24.kt
@@ -54,13 +54,16 @@ object Day24 {
 
     fun part1(input: List<String>, min: Long, max: Long): Int {
         val range = min.toDouble()..max.toDouble()
-        val hailstones = input.map { Hailstone.fromString(it) }
-        return hailstones.asSequence().withEachOf(hailstones.asSequence()).filter { it.first != it.second }.filter {
-            val intersection = it.first.intersect(it.second)
-            intersection != null
-                    && intersection.first in range && intersection.second in range
-                    && !it.first.inThePast(intersection) && !it.second.inThePast(intersection)
-        }.count() / 2
+        val hailstones = input.map { Hailstone.fromString(it) }.asSequence()
+        return hailstones.withEachOf(hailstones)
+            .filter { it.first != it.second }
+            .filter {
+                val intersection = it.first.intersect(it.second)
+                intersection != null // they do intersect...
+                        && intersection.first in range && intersection.second in range // ...in the given range
+                        && !it.first.inThePast(intersection) && !it.second.inThePast(intersection)// ...in the future
+            }
+            .count() / 2 // if A hits B, then B also hits A -> divide result by 2
     }
 
     fun part2(input: List<String>): Int {
