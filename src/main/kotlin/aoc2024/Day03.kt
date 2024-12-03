9eb4d6513012909c68760bc78e447aa8d698b4db
Thomas Hoffmann <thomas.hoffmann2@rewe-group.com>
2024-12-03 07:57:56 +0100

    day03 solution

--- src/main/kotlin/aoc2024/Day03.kt
+++ src/main/kotlin/aoc2024/Day03.kt
@@ -3,21 +3,35 @@ package aoc2024
 import readInput
 
 object Day03 {
-	fun part1(input: List<String>): Int {
-		return 0
-	}
 
-	fun part2(input: List<String>): Int {
-		return 0
-	}
+    fun part1(input: String): Int {
+        val regex = """mul\((\d{1,3}),(\d{1,3})\)""".toRegex()
+        return regex.findAll(input)
+            .map { it.groups[1]!!.value.toInt() to it.groups[2]!!.value.toInt() }
+            .sumOf { it.first * it.second }
+    }
+
+    fun part2(input: String): Int {
+        val regex = """(mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\))""".toRegex()
+        var enabled = true
+        return regex.findAll(input).mapNotNull {
+            when (it.value) {
+                "do()" -> enabled = true
+                "don't()" -> enabled = false
+                else -> if (enabled) {
+                    return@mapNotNull it
+                }
+            }
+            null
+        }.map { it.groups[2]!!.value.toInt() to it.groups[3]!!.value.toInt() }.sumOf { it.first * it.second }
+    }
 }
 
 fun main() {
-	val testInput = readInput("Day03_test", 2024)
-	check(Day03.part1(testInput) == 0)
-	check(Day03.part2(testInput) == 0)
+    check(Day03.part1("""xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))""") == 161)
+    check(Day03.part2("""xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))""") == 48)
 
-	val input = readInput("Day03", 2024)
-	println(Day03.part1(input))
-	println(Day03.part2(input))
+    val input = readInput("Day03", 2024).joinToString(separator = "")
+    println(Day03.part1(input))
+    println(Day03.part2(input))
 }
