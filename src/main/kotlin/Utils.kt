e365d82150165c1ade0c5d0b253658a9e7e6db43
Thomas Hoffmann <thomas.hoffmann2@rewe-group.com>
2024-12-10 07:49:23 +0100

    day10 solution

--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -243,8 +243,8 @@ fun Array<CharArray>.findAll(chars: CharArray): Map<PointL, Char> {
     val maxX = first().size - 1
     val maxY = size - 1
     val map = mutableMapOf<PointL, Char>()
-    for (x in 0..<maxX) {
-        for (y in 0..<maxY) {
+    for (x in 0..maxX) {
+        for (y in 0..maxY) {
             if (this[x][y] in chars) {
                 map[PointL(x, y)] = this[x][y]
             }
@@ -272,6 +272,50 @@ inline fun <reified T : Any> List<String>.to2dArray(ignore: Char? = null, mapper
 fun List<String>.to2dIntArray(ignore: Char? = null) =
     to2dCharArray(ignore).map { chars -> chars.map { it.digitToInt() }.toIntArray() }.toTypedArray()
 
+fun Array<CharArray>.get(x: Long, y: Long): Char = this[x.toInt()][y.toInt()]
+
+/**
+ * Simple recursive breadth first search
+ *
+ * @param start                 the starting point
+ * @param withDiagonal          true, to also allow diagonal paths (default: false)
+ * @param validGrid             the grid to search for neighbours in (default: whole map)
+ * @param neighbourCondition    optional condition to only allow specific neighbours.
+ *                              Receives the current and the potential neighbour as parameter
+ * @param targetCondition       condition to determine the "end" of the search.
+ *                              Receives the current position as parameter
+ *
+ * @return a list of paths (=list of positions) from [start] to points meeting the [targetCondition]
+ */
+fun Array<CharArray>.bfs(
+    start: PointL,
+    withDiagonal: Boolean = false,
+    validGrid: Pair<PointL, PointL> = Pair(PointL(0, 0), PointL(maxX, maxY)),
+    neighbourCondition: (PointL, PointL) -> Boolean = { _, _ -> true },
+    targetCondition: (PointL) -> Boolean,
+): List<List<PointL>> {
+    return bfsRec(emptyList(), start, targetCondition, neighbourCondition, withDiagonal, validGrid)
+}
+
+private fun Array<CharArray>.bfsRec(
+    pathSoFar: List<PointL>,
+    current: PointL,
+    targetCondition: (PointL) -> Boolean,
+    neighbourCondition: (PointL, PointL) -> Boolean,
+    withDiagonal: Boolean,
+    validGrid: Pair<PointL, PointL>,
+): List<List<PointL>> {
+    return if (targetCondition(current)) {
+        listOf(pathSoFar + current)
+    } else {
+        val neighbours = current.getNeighbours(withDiagonal, validGrid).filter { neighbourCondition(current, it) }
+        neighbours.flatMap {
+            bfsRec(pathSoFar + current, it, targetCondition, neighbourCondition, withDiagonal, validGrid)
+        }
+    }
+}
+
+
 fun Array<CharArray>.print() {
     val maxX = size - 1
     val maxY = first().size - 1
@@ -291,8 +335,20 @@ fun LongRange.cut(other: LongRange): Sequence<LongRange> = sequence {
     when {
         other.last < this@cut.first || this@cut.last < other.first -> yield(this@cut)  // completely out of cut
         other.first <= this@cut.first && this@cut.last <= other.last -> {} // completely within cut
-        other.first <= this@cut.first && this@cut.last > other.last -> yield(LongRange(other.last + 1, this@cut.last))
-        other.first > this@cut.first && this@cut.last <= other.last -> yield(LongRange(this@cut.first, other.first - 1))
+        other.first <= this@cut.first && this@cut.last > other.last -> yield(
+            LongRange(
+                other.last + 1,
+                this@cut.last
+            )
+        )
+
+        other.first > this@cut.first && this@cut.last <= other.last -> yield(
+            LongRange(
+                this@cut.first,
+                other.first - 1
+            )
+        )
+
         this@cut.first < other.first && other.last < this@cut.last -> {
             yield(LongRange(this@cut.first, other.first - 1))
             yield(LongRange(other.last + 1, this@cut.last))
--- src/main/kotlin/aoc2024/Day10.kt
+++ src/main/kotlin/aoc2024/Day10.kt
@@ -1,23 +1,48 @@
 package aoc2024
 
+import PointL
+import bfs
+import findAll
+import get
 import readInput
+import to2dCharArray
+
+typealias Path = List<PointL>
 
 object Day10 {
-	fun part1(input: List<String>): Int {
-		return 0
-	}
 
-	fun part2(input: List<String>): Int {
-		return 0
-	}
+    private fun getScores(input: List<String>, scoreFunction: (List<Path>) -> Int): List<Int> {
+        val map = input.to2dCharArray()
+        val startingPoints = map.findAll("0".toCharArray()).keys
+        val scores = startingPoints.map {
+            val paths = map.bfs(
+                start = it,
+                withDiagonal = false,
+                neighbourCondition = { current, next ->
+                    map.get(current.x, current.y).digitToInt() + 1 == map.get(next.x, next.y).digitToInt()
+                }, targetCondition = { current ->
+                    map.get(current.x, current.y) == '9'
+                })
+            scoreFunction(paths)
+        }
+        return scores
+    }
+
+    fun part1(input: List<String>): Int {
+        return getScores(input) { it.map { it.last() }.distinct().size }.sum()
+    }
+
+    fun part2(input: List<String>): Int {
+        return getScores(input) { it.size }.sum()
+    }
 }
 
 fun main() {
-	val testInput = readInput("Day10_test", 2024)
-	check(Day10.part1(testInput) == 0)
-	check(Day10.part2(testInput) == 0)
+    val testInput = readInput("Day10_test", 2024)
+    check(Day10.part1(testInput) == 36)
+    check(Day10.part2(testInput) == 81)
 
-	val input = readInput("Day10", 2024)
-	println(Day10.part1(input))
-	println(Day10.part2(input))
+    val input = readInput("Day10", 2024)
+    println(Day10.part1(input))
+    println(Day10.part2(input))
 }
