9a59d0d129abaa74ad3b2aa7c5ea0676f86aeeb7
Thomas Hoffmann <thomas.hoffmann2@rewe-group.com>
2024-12-12 11:32:43 +0100

    day12 pt1

--- src/main/kotlin/aoc2024/Day12.kt
+++ src/main/kotlin/aoc2024/Day12.kt
@@ -1,23 +1,77 @@
 package aoc2024
 
+import PointL
+import get
+import grid
 import readInput
+import to2dCharArray
+import java.util.LinkedList
+import java.util.Queue
 
 object Day12 {
-	fun part1(input: List<String>): Int {
-		return 0
-	}
 
-	fun part2(input: List<String>): Int {
-		return 0
-	}
+    private data class Area(val char: Char, val points: MutableSet<PointL> = mutableSetOf()) {
+
+        val area: Int
+            get() = points.size
+
+        val perimeter: Int
+            get() = points.map { current ->
+                val neighbours = current.getNeighbours(withDiagonal = false).filter { points.contains(it) }.size
+                4 - neighbours
+            }.sum()
+
+        val sides: Int
+            get() {
+                return 0
+            }
+    }
+
+    private fun getAreas(input: List<String>): Set<Area> {
+        val map = input.to2dCharArray()
+        val visitedPoints = mutableSetOf<PointL>()
+        val areas = mutableSetOf<Area>()
+        val grid = map.grid
+        for (x in map.indices) {
+            for (y in map[0].indices) {
+                val current = PointL(x, y)
+                if (!visitedPoints.contains(current)) {
+                    val currentType = map[x][y]
+                    val newArea = Area(currentType)
+                    areas.add(newArea)
+                    val toVisit: Queue<PointL> = LinkedList(listOf(current))
+
+                    while (toVisit.isNotEmpty()) {
+                        val next = toVisit.remove()
+                        newArea.points.add(next)
+                        visitedPoints.add(next)
+                        val toAdd = next.getNeighbours(withDiagonal = false, validGrid = grid)
+                            .filter { map.get(it.x, it.y) == currentType }
+                            .filter { !visitedPoints.contains(it) }
+                            .filter { !toVisit.contains(it) }
+                        toVisit.addAll(toAdd)
+                    }
+                }
+            }
+        }
+        return areas
+    }
+
+    fun part1(input: List<String>): Int {
+        return getAreas(input).sumOf { it.area * it.perimeter }
+    }
+
+    fun part2(input: List<String>): Int {
+        return getAreas(input).sumOf { it.area * it.sides }
+    }
 }
 
 fun main() {
-	val testInput = readInput("Day12_test", 2024)
-	check(Day12.part1(testInput) == 0)
-	check(Day12.part2(testInput) == 0)
+    val testInput = readInput("Day12_test", 2024)
+    check(Day12.part1(testInput) == 1930)
+    check(Day12.part2(testInput) == 1206)
 
-	val input = readInput("Day12", 2024)
-	println(Day12.part1(input))
-	println(Day12.part2(input))
+    val input = readInput("Day12", 2024)
+    println(Day12.part1(input))
+    println(Day12.part2(input))
 }
