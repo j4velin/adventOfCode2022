66b339d9d84baa8b176e9b7f688c3dab9b3bdb7f
Thomas Hoffmann <thomas.hoffmann2@rewe-group.com>
2024-12-13 11:15:13 +0100

    day13 pt1

--- src/main/kotlin/aoc2024/Day13.kt
+++ src/main/kotlin/aoc2024/Day13.kt
@@ -1,23 +1,109 @@
 package aoc2024
 
+import PointL
 import readInput
+import separateBy
+import kotlin.math.floor
+import kotlin.math.min
 
 object Day13 {
-	fun part1(input: List<String>): Int {
-		return 0
-	}
 
-	fun part2(input: List<String>): Int {
-		return 0
-	}
+    private data class Machine(val buttonA: Button, val buttonB: Button, val prize: PointL) {
+        companion object {
+            fun fromString(input: List<String>): Machine {
+                val buttonA = Button.fromString(input[0])
+                val buttonB = Button.fromString(input[1])
+                val prizePosition = input[2].split(": ").last().split(", ").map { it.substring(2).toInt() }
+
+                return Machine(buttonA, buttonB, PointL(prizePosition[0], prizePosition[1]))
+            }
+        }
+
+        fun priceToReachPrize(offset: Long = 0L): Long? {
+            /**
+             * a * ax + b * bx = px    --> a = (px - (b * bx)) / ax
+             * a * ay + b * by = py    --> a = (py - (b * by)) / ay
+             *
+             *                         --> (px - b * bx) / ax = (py - b * by) / ay
+             *                         --> (px - b * bx) * ay = (py - b * by) * ax
+             *                         --> (px * ay) - (b * bx * ay) = (py * ax) - (b * by * ax)
+             *                         --> (px * ay) - (py * ax) = (b * bx * ay) - (b * by * ax)
+             *                         --> (px * ay) - (py * ax) = b * (bx * ay - by * ax)
+             *                         --> b = ((px * ay) - (py * ax)) / (bx * ay - by * ax)
+             *
+             *
+             *                         --> (px - a * ax) / bx = (py - a * ay) / by
+             *                         --> (px - a * ax) * by = (py - a * ay) * bx
+             *                         --> by * px - by * a * ax = bx * py - bx * a * ay
+             *                         --> bx * a * ay - by * a * ax = bx * py - by * px
+             *                         --> a * (bx * ay - by * ax) = bx * py - by * px
+             *                         --> a = (bx * py - by * px) / (bx * ay - by * ax)
+             */
+            val ax = buttonA.delta.x
+            val bx = buttonB.delta.x
+            val px = prize.x + offset
+            val ay = buttonA.delta.y
+            val by = buttonB.delta.y
+            val py = prize.y + offset
+
+            var minCost: Long = Long.MAX_VALUE
+            val b = ((px * ay) - (py * ax)).toDouble() / (bx * ay - by * ax).toDouble()
+            if (floor(b) == b) {
+                /**
+                 * a * ax + b * bx = px
+                 * --> a * ax = px - b * bx
+                 * --> a = (px - b * bx) / ax
+                 */
+                val a = (px - b.toLong() * bx) / ax
+                //println("solvable with a=$a")
+                minCost = min(minCost, a * 3 + b.toLong())
+            }
+
+            val a = (bx * py - by * px).toDouble() / (bx * ay - by * ax).toDouble()
+            if (floor(a) == a) {
+                /**
+                 * a * ax + b * bx = px
+                 * --> b * bx = px - a * ax
+                 * --> b = (px - a * ax) / bx
+                 */
+                val b = (px - a.toLong() * ax) / bx
+                //println("solvable with a=${a.toLong()}")
+                minCost = min(minCost, a.toLong() * 3 + b)
+            }
+
+            return if (minCost == Long.MAX_VALUE) null else minCost
+        }
+    }
+
+    private data class Button(val cost: Int, val delta: PointL) {
+        companion object {
+            fun fromString(s: String): Button {
+                val split = s.split(": ")
+                val cost = if (split.first().last() == 'A') 3 else 1
+                val deltas = split.last().split(", ").map { it.substring(1).toInt() }
+                val delta = PointL(deltas[0], deltas[1])
+                return Button(cost, delta)
+            }
+        }
+    }
+
+    fun part1(input: List<String>): Long {
+        val machines = input.separateBy { it.isEmpty() }.map { Machine.fromString(it) }
+        return machines.mapNotNull { machine -> machine.priceToReachPrize() }.sum().toLong()
+    }
+
+    fun part2(input: List<String>): Long {
+        val machines = input.separateBy { it.isEmpty() }.map { Machine.fromString(it) }
+        return machines.mapNotNull { machine -> machine.priceToReachPrize(10000000000000L) }.sum()
+    }
 }
 
 fun main() {
-	val testInput = readInput("Day13_test", 2024)
-	check(Day13.part1(testInput) == 0)
-	check(Day13.part2(testInput) == 0)
+    val testInput = readInput("Day13_test", 2024)
+    check(Day13.part1(testInput) == 480L)
+    //check(Day13.part2(testInput) == 0L)
 
-	val input = readInput("Day13", 2024)
-	println(Day13.part1(input))
-	println(Day13.part2(input))
+    val input = readInput("Day13", 2024)
+    println(Day13.part1(input))
+    println(Day13.part2(input))
 }
