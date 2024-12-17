79125932561126c56808b0a9b0f17373801b9e0b
Thomas Hoffmann <git@j4velin.de>
2024-12-17 21:04:30 +0100

    pt2 solution (not a general one but for my input, good enough for now)

--- src/main/kotlin/aoc2024/Day17.kt
+++ src/main/kotlin/aoc2024/Day17.kt
@@ -1,13 +1,15 @@
 package aoc2024
 
-import modulo
 import readInput
-import kotlin.math.floor
 import kotlin.math.pow
 
 object Day17 {
 
-    private data class Computer(private var registerA: Long, private var registerB: Long, private var registerC: Long) {
+    private data class Computer(
+        private var registerA: Long,
+        private var registerB: Long = 0L,
+        private var registerC: Long = 0L
+    ) {
 
         companion object {
             fun fromString(input: List<String>): Computer {
@@ -30,7 +32,7 @@ object Day17 {
             else -> throw IllegalArgumentException("Invalid combo operand: $operand")
         }
 
-        fun runProgram(program: List<Int>): String {
+        fun runProgram(program: List<Int>, expectedOutput: List<Long>? = null): List<Long> {
 
             val output = mutableListOf<Long>()
 
@@ -58,14 +60,18 @@ object Day17 {
                     }
 
                     Instruction.BXC -> registerB = registerB xor registerC
-                    Instruction.OUT -> output.add(getComboOperand(operand) % 8)
+                    Instruction.OUT -> {
+                        output.add(getComboOperand(operand) % 8)
+                        // early return for pt 2
+                        if (expectedOutput != null && expectedOutput.take(output.size) != output) {
+                            return emptyList()
+                        }
+                    }
                 }
                 instructionPointer += 2
             }
-
-            return output.joinToString(separator = ",")
+            return output
         }
-
     }
 
     private enum class Instruction(val opcode: Int) {
@@ -87,18 +93,55 @@ object Day17 {
     fun part1(input: List<String>): String {
         val computer = Computer.fromString(input.take(3))
         val program = input.last().split(" ").last().split(",").map { it.toInt() }
-        return computer.runProgram(program)
+        return computer.runProgram(program).joinToString(separator = ",")
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part2(input: List<String>): Long {
+        // handcrafted for my specific input:
+        // (1) 2,4 -> b = a % 8
+        // (2) 1,1 -> b = b xor 1
+        // (3) 7,5 -> c = a / 2^b
+        // (4) 0,3 -> a = a / 2^3
+        // (5) 4,3 -> b = b xor c
+        // (6) 1,6 -> b = b xor 6
+        // (7) 5,5 -> output: b % 8
+        // (8) 3,0 -> a != 0 ? jmp to 0 again
+
+        // we output the last 3 bits of 'b' in each iteration
+        // a is divided by 8 in each iteration
+        // 1 output per iteration -> 16 iterations
+        // a must be 0 at the end of the 16th iteration
+        // last output must be "0"
+
+        val program = input.last().split(" ").last().split(",").map { it.toInt() }
+        val simulate: (Long, List<Long>) -> Long = { expectedA, expectedOutput ->
+            // (4) a = a / 2^3
+            // --> a / 8 >= expectedA
+            // --> a >= expectedA * 8
+            var a = expectedA * 8 - 1   // -1 because we start our loop with a++
+            var result = listOf<Long>()
+            while (result != expectedOutput) {
+                a++
+                val computer = Computer(registerA = a)
+                result = computer.runProgram(program, expectedOutput)
+            }
+            a
+        }
+
+        val expectedOutput = mutableListOf<Long>()
+        var lastResult = 0L
+        program.reversed().forEach { digit ->
+            expectedOutput.addFirst(digit.toLong())
+            lastResult = simulate(lastResult, expectedOutput)
+        }
+
+        return lastResult
     }
 }
 
 fun main() {
     val testInput = readInput("Day17_test", 2024)
     check(Day17.part1(testInput) == "4,6,3,5,6,3,5,2,1,0")
-    check(Day17.part2(testInput) == 0)
 
     val input = readInput("Day17", 2024)
     println(Day17.part1(input))
